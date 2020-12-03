44a9e87c5ede005f12112ab1c3d1cd32128ddb45
nurkyz1 <nurkyz.esengulova020102@gmail.com>
2020-12-03 19:21:56 +0600

    homework

--- src/kg/geektech/les8/players/Witcher.java
+++ src/kg/geektech/les8/players/Witcher.java
@@ -1,12 +1,21 @@
 package kg.geektech.les8.players;
 
+import kg.geektech.les8.game.RPG_Game;
+
 public class Witcher extends Hero {
-    public Witcher(int health, int damage, SuperAbility superAbility) {
-        super(health, damage, superAbility);
+    public Witcher(int health, int damage) {
+        super(health, damage,SuperAbility.HEAL_THE_HERO);
     }
 
     @Override
     public void applySuperPower(Boss boss, Hero[] heroes) {
+        boolean heal = RPG_Game.random.nextBoolean();
+        for (int i = 0; i <heroes.length ; i++) {
+          if (heroes[i].getHealth()<=0&& heal==true){
+             heroes[i].setHealth(heroes[i].getHealth()+heroes[i].getHealth());
+             this.setHealth(0);
+          }
+        }
 
     }
 }
